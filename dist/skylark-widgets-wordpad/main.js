f30c2e4cebbe6fe326768a46899860732586a7ab
hudaokeji <github@hudaokeji.com>
2021-08-29 18:42:13 +0900

    rename module file name

---
+++ dist/skylark-widgets-wordpad/main.js
@@ -0,0 +1,9 @@
+/**
+ * skylark-widgets-wordpad - The skylark richeditor widget
+ * @author Hudaokeji, Inc.
+ * @version v0.9.0
+ * @link https://github.com/skylark-widgets/skylark-widgets-wordpad/
+ * @license MIT
+ */
+define(["./wordpad","./action","./popover","./addons/actions/alignment-action","./addons/actions/blockquote-action","./addons/actions/bold-action","./addons/actions/code-action","./addons/actions/code-popover","./addons/actions/color-action","./addons/actions/emoji-action","./addons/actions/font-scale-action","./addons/actions/fullscreen-action","./addons/actions/hr-action","./addons/actions/html-action","./addons/actions/image-action","./addons/actions/image-popover","./addons/actions/indent-action","./addons/actions/italic-action","./addons/actions/link-action","./addons/actions/link-popover","./addons/actions/list-action","./addons/actions/mark-action","./addons/actions/order-list-action","./addons/actions/outdent-action","./addons/actions/strikethrough-action","./addons/actions/table-action","./addons/actions/title-action","./addons/actions/underline-action","./addons/actions/unorder-list-action","./addons/actions/video-action","./addons/toolbar/items/alignment-button","./addons/toolbar/items/color-button","./addons/toolbar/items/emoji-button","./addons/toolbar/items/table-button","./addons/toolbar/items/title-button"],function(o){return o});
+//# sourceMappingURL=sourcemaps/main.js.map
---
+++ dist/uncompressed/skylark-widgets-wordpad/main.js
@@ -0,0 +1,42 @@
+define([
+  "./wordpad", 
+  "./action",
+  "./popover",
+
+  "./addons/actions/alignment-action", 
+  "./addons/actions/blockquote-action", 
+  "./addons/actions/bold-action", 
+  "./addons/actions/code-action", 
+  "./addons/actions/code-popover", 
+  "./addons/actions/color-action", 
+  "./addons/actions/emoji-action", 
+  "./addons/actions/font-scale-action", 
+  "./addons/actions/fullscreen-action", 
+  "./addons/actions/hr-action", 
+  "./addons/actions/html-action", 
+  "./addons/actions/image-action", 
+  "./addons/actions/image-popover", 
+  "./addons/actions/indent-action", 
+  "./addons/actions/italic-action", 
+  "./addons/actions/link-action", 
+  "./addons/actions/link-popover", 
+  "./addons/actions/list-action", 
+  "./addons/actions/mark-action", 
+  "./addons/actions/order-list-action", 
+  "./addons/actions/outdent-action",
+  "./addons/actions/strikethrough-action", 
+  "./addons/actions/table-action", 
+  "./addons/actions/title-action", 
+  "./addons/actions/underline-action", 
+  "./addons/actions/unorder-list-action",
+  "./addons/actions/video-action",
+
+  "./addons/toolbar/items/alignment-button",
+  "./addons/toolbar/items/color-button",
+  "./addons/toolbar/items/emoji-button",
+  "./addons/toolbar/items/table-button",
+  "./addons/toolbar/items/title-button"
+],function(Wordpad){
+	
+  return Wordpad;
+});
